92cd2d6a911bbed5d5b47ebd2820802587b1c99d
Nichoas Ito <nicholas.ito94@gmail.com>
2023-08-16 14:40:34 -0300

    icone do github

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { useState } from "react"
 import Perfil from "./components/Perfil"
 // import Formulario from "./components/Formulario"
 import ReposList from "./components/ReposList"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
 function App() {
   const [formularioVisivel, setFormularioVisivel] = useState(true)
@@ -10,6 +12,9 @@ function App() {
   return (
     <>
       <div className="container">
+      <div className="githubIconWrapper">
+        <FontAwesomeIcon icon={faGithub} size="6x" />
+      </div>
         <div className="inputContainer">
           <input type="text" onBlur={(e) => setNomeUsuario(e.target.value)} />
           <button onClick={() => setFormularioVisivel(!formularioVisivel)} type="button">
