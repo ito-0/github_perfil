86c8a8468c6075e81683defeac7ee6d634f1c2c6
Nichoas Ito <nicholas.ito94@gmail.com>
2023-08-16 09:36:14 -0300

    Adicionando novas funcionalidades

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,28 @@
 import { useState } from "react"
 
 import Perfil from "./components/Perfil"
-import Formulario from "./components/Formulario"
+// import Formulario from "./components/Formulario"
 import ReposList from "./components/ReposList"
 
 function App() {
   const [formularioVisivel, setFormularioVisivel] = useState(true)
-  const [ nomeUsuario, setNomeUsuario] = useState('')
+  const [nomeUsuario, setNomeUsuario] = useState('')
   return (
-    <> 
-      <input type="text" onBlur={(e) => setNomeUsuario(e.target.value)} />
-      
-
-      {nomeUsuario.length > 2 && (
-        <>
-          <Perfil nomeUsuario={nomeUsuario}/>
-          <ReposList nomeUsuario={nomeUsuario}/>
-        </>
-      )}
-      
-      {formularioVisivel && (
-        <Formulario />
-      )}
-
-      <button onClick={() => setFormularioVisivel(!formularioVisivel)} type="button">toggle form</button>
+    <>
+      <div className="container">
+        <div className="inputContainer">
+          <input type="text" onBlur={(e) => setNomeUsuario(e.target.value)} />
+          <button onClick={() => setFormularioVisivel(!formularioVisivel)} type="button">
+            Buscar
+          </button>
+        </div>
+        {nomeUsuario.length > 2 && (
+          <>
+            <Perfil nomeUsuario={nomeUsuario} />
+            <ReposList nomeUsuario={nomeUsuario} />
+          </>
+        )}
+      </div>
     </>
   )
 }
--- src/components/Perfil/index.tsx
+++ src/components/Perfil/index.tsx
@@ -1,3 +1,6 @@
+import React, { useState, useEffect } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
 import styles from './Perfil.module.css'
 
 interface PerfilProps {
@@ -5,13 +8,50 @@ interface PerfilProps {
 }
 
 const Perfil: React.FC<PerfilProps> = ({ nomeUsuario }) => {
+    const [avatarErro, setAvatarErro] = useState(false);
+    const [carregadoComSucesso, setCarregadoComSucesso] = useState(false);
+
+    useEffect(() => {
+        setAvatarErro(false);
+        setCarregadoComSucesso(false);
+
+        const img = new Image();
+        img.src = `https://github.com/${nomeUsuario}.png`;
+
+        img.onload = () => {
+            setTimeout(() => {
+                setCarregadoComSucesso(true);
+            }, 3000);
+        };
+
+        img.onerror = () => {
+            setAvatarErro(true);
+        };
+    }, [nomeUsuario]);
+
     return (
         <header className={styles.header}>
-            <img className={styles.avatar} src={`https://github.com/${nomeUsuario}.png`} alt={`${nomeUsuario}'s avatar`} />
-            <h1 className={styles.name}>{nomeUsuario}</h1>
+            {avatarErro ? (
+                <a className={styles.signupButton} href={`https://github.com/signup`} target="_blank" rel="noopener noreferrer">GitHub sign up</a>
+            ) : (
+                <>
+                    {carregadoComSucesso ? (
+                        <>
+                            <img
+                                className={styles.avatar}
+                                src={`https://github.com/${nomeUsuario}.png`}
+                                alt={`${nomeUsuario}'s avatar`}
+                            />
+                            <h1 className={styles.name}>{nomeUsuario}</h1>
+                        </>
+                    ) : (
+                        <div className={styles.loading}>
+                            <FontAwesomeIcon icon={faSpinner} spin size="2x" />
+                        </div>
+                    )}
+                </>
+            )}
         </header>
-
-        
     );
 }
 
